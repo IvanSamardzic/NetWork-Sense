1aba06f66bda607fb0c7152e0512c2608e57070b
Ivan Samardzic <56680446+IvanSamardzic@users.noreply.github.com>
2020-11-21 18:39:34 +0100

    Add files via upload

---
+++ hardware-tests/ultrasonic-sensor-tests/ultrasonic-sensor-testing.ino
@@ -0,0 +1,21 @@
+const int trigPin = 9;
+const int echoPin = 10;
+int duration, distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+}
